8788789856bc56201baa81d4991d9bfec5a0902f
cloudsheger <cloudsheger@gmail.com>
2023-11-07 20:53:48 -0500

    updated jenkinsfile

--- start-jenkins.sh
+++ start-jenkins.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Create a Docker volume for Jenkins data
 docker volume create jenkins-data
+docker network create jenkins-network
 
 # Run the jenkins-docker container with a mounted volume for data persistence
-docker run --name jenkins-docker -d \
+docker run --name jenkins-docker --network jenkins-network -d \
  -u root -p 8080:8080 -v /var/run/docker.sock:/var/run/docker.sock \
  -v jenkins-data:/var/jenkins_home \
  cloudsheger/jenkins-docker-latest:latest
